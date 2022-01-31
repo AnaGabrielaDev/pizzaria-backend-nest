25aaf2de782b183994d4aa4688f4164a217987fc
Ana Gabriela <anagabrieladev@gmail.com>
2022-01-30 22:34:47 -0300

    refactor: unsed  get

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,13 +5,4 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
-
-  getObject(): any {
-    let object = {
-      id: 1,
-      name: "asdasd" 
-    }
-
-    return object;
-  }
 }
